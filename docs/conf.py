2791735196d7149fddeb1144ad2af741ef5145d2
gamcil <clmgilchrist@gmail.com>
2019-09-07 12:09:51 +0800

    set master to index

--- docs/conf.py
+++ docs/conf.py
@@ -27,16 +27,12 @@ release = "1.0"
 
 
 # -- General configuration ---------------------------------------------------
+master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
