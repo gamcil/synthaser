355a25011ad7e17c68141f514396fd03194c437f
gamcil <clmgilchrist@gmail.com>
2019-11-15 13:22:18 +0800

    add test dependencies, entry point to CLI

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as readme:
+    long_description = readme.read()
+
 setup(
     name="synthaser",
     author="Cameron Gilchrist",
-    version="0.0",
+    version="0.0.2",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gamcil/synthaser",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"],
+    python_requires=">=3.6",
+    tests_require=["pytest", "pytest-cov", "pytest-mock", "requests-mock"],
+    entry_points={"console_scripts": ["synthaser=synthaser.main:main"]},
 )
