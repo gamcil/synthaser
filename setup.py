255456b06cf5c45ccb0e7fff9bb0697caf1477e1
gamcil <clmgilchrist@gmail.com>
2019-11-15 13:52:36 +0800

    add function for finding version number

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+import re
+
+from pathlib import Path
 from setuptools import setup, find_packages
 
 with open("README.md") as readme:
     long_description = readme.read()
 
+
+def get_version():
+    """Get version number from __init__.py"""
+    version_file = Path(__file__).resolve().parent / "clusterblaster" / "__init__.py"
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read_text(), re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Failed to find version string")
+
+
 setup(
     name="synthaser",
     author="Cameron Gilchrist",
-    version="0.0.2",
+    version=get_version(),
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
