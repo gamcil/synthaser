0cfd86106cd664f4097db402cf0bd3e4d49d76fc
gamcil <clmgilchrist@gmail.com>
2019-12-06 13:51:50 +0800

    remove unecessary imports after refactor

--- synthaser/ncbi.py
+++ synthaser/ncbi.py
@@ -76,13 +76,9 @@ import time
 import logging
 import re
 
-from pathlib import Path
-
 import requests
 
-from synthaser import fasta, results
-from synthaser.classify import classify
-from synthaser.models import SynthaseContainer
+from synthaser import fasta
 
 LOG = logging.getLogger(__name__)
 
@@ -101,61 +97,6 @@ SEARCH_PARAMS = {
     "dmode": "full",
     "tdata": "hits",
 }
-
-SEARCH_HISTORY = []
-
-
-def _container_from_query_file(handle):
-    """Build SynthaseContainer from FASTA file handle."""
-    return SynthaseContainer.from_sequences(fasta.parse(handle))
-
-
-def _container_from_query_ids(ids):
-    """Build SynthaseContainer from query ID file or collection.
-
-    First checks if `ids` is an iterable; if so, fetch sequences from NCBI and return a
-    new SynthaseContainer. Otherwise, expects a file containing a collection of IDs
-    each on a new line.
-    """
-    if not hasattr(ids, "__iter__"):
-        raise ValueError("Expected iterable")
-
-    if len(ids) == 1 and Path(ids[0]).exists():
-        # This is a file
-        with Path(ids[0]).open() as fp:
-            _ids = [line.strip() for line in fp]
-        return SynthaseContainer.from_sequences(efetch_sequences(_ids))
-
-    # Otherwise, expect nargs with IDs
-    return SynthaseContainer.from_sequences(efetch_sequences(ids))
-
-
-def prepare_input(query_ids=None, query_file=None):
-    """Generate a SynthaseContainer from either query IDs or a query file.
-
-    Returns
-    -------
-    models.SynthaseContainer
-        Collection of `models.Synthase` objects representing query sequences.
-
-    Raises
-    ------
-    ValueError
-        Neither `query_ids` nor `query_file` provided
-    ValueError
-        Too many sequences were provided (NCBI limits searches at 4000 sequences)
-    """
-    if query_ids:
-        container = _container_from_query_ids(query_ids)
-    elif query_file:
-        container = _container_from_query_file(query_file)
-    else:
-        raise ValueError("Expected 'query_ids' or 'query_file'")
-
-    if len(container) > 4000:
-        raise ValueError("Too many sequences (NCBI limit = 4000)")
-
-    return container
 
 
 def launch(query):
@@ -337,118 +278,6 @@ def retrieve(cdsid, max_retries=-1, delay=20):
     return response
 
 
-def history():
-    """Print out summary of previously saved CD-Search runs.
-
-    The parameters and results of prior searches are stored in `SEARCH_HISTORY`, which
-    is simply a list of dictionaries:
-
-    >>> ncbi.SEARCH_HISTORY
-    [{'cdsid': 'QM3-qcdsearch-C7558163144A288-147D9ECB39A09F8B',
-    'parameters': {'db': 'cdd', 'smode': 'auto', 'useid1': 'true', 'compbasedadj': '1',
-    'filter': 'true', 'evalue': '3.0', 'maxhit': '500', 'dmode': 'full', 'tdata':
-    'hits'}, 'results': <Response [200]>}]
-
-    This function simply formats the entries in `SEARCH_HISTORY` nicely:
-
-    >>> ncbi.history()
-    1.      Run ID: QM3-qcdsearch-B4BAD4B59BC5B80-3E7CFCD3F93E21D0
-        Parameters:
-                    db: cdd
-                 smode: auto
-                useid1: true
-          compbasedadj: 1
-                filter: true
-                evalue: 3.0
-                maxhit: 500
-                 dmode: full
-                 tdata: hits
-
-    Raises
-    ------
-    ValueError
-        If `SEARCH_HISTORY` is empty (i.e. no searches have been run).
-    """
-    if not SEARCH_HISTORY:
-        raise ValueError("No searches have been run")
-
-    slen = max(len(key) for key in SEARCH_PARAMS)
-
-    for index, run in enumerate(SEARCH_HISTORY, 1):
-        print(
-            f"{f'{index}.':8}Run ID: {run['cdsid']}\n    Parameters:\n"
-            + "\n".join(
-                f"{setting:>{slen+6}}: {value}"
-                for setting, value in run["parameters"].items()
-            )
-        )
-
-
-def CDSearch(
-    query_file=None,
-    query_ids=None,
-    cdsid=None,
-    output=None,
-    delay=20,
-    max_retries=-1,
-    domain_file=None,
-):
-    """Launch a new CD-Search job.
-
-    This function takes a query FASTA file or collection of NCBI sequence
-    identifiers, sends them to the CD-Search server and then polls for response until
-    one is returned.
-
-    >>> results = CDSearch(query_file='path/to/query.fasta')
-    >>> results = CDSearch(query_ids=['XP_000000001.1', ... ])
-    >>> results
-    <Response [200]>
-
-    Search parameters are specified in `SEARCH_PARAMS`. For example, to adjust the
-    e-value cutoff used in the search, simply change it there:
-
-    >>> SEARCH_PARAMS['evalue'] = 2.0
-
-    Then, future searches will use the updated value.
-    """
-    query = prepare_input(query_ids, query_file)
-
-    if not cdsid:
-        cdsid = launch(query)
-
-    LOG.info("Run ID: %s", cdsid)
-    LOG.info("Polling NCBI for results...")
-    response = retrieve(cdsid, delay=delay, max_retries=max_retries)
-
-    entry = {
-        "cdsid": cdsid,
-        "parameters": SEARCH_PARAMS,
-        "query": query,
-        "results": results,
-    }
-
-    SEARCH_HISTORY.append(entry)
-
-    if output:
-        LOG.info("Writing CD-Search results table to %s", output)
-        with open(output, "w") as handle:
-            handle.write(response.text)
-
-    if domain_file:
-        LOG.info("Reading domain rules from: %s", domain_file.name)
-        results.load_domain_json(domain_file)
-
-    LOG.info("Parsing results for domains...")
-    for header, domains in results.parse(response.text.split("\n")).items():
-        query.get(header).domains = domains
-
-    LOG.info("Classifying synthases...")
-    for synthase in query:
-        classify(synthase)
-
-    return query
-
-
 def efetch_sequences(headers):
     """Retrieve protein sequences from NCBI for supplied accessions.
 
@@ -484,7 +313,7 @@ def efetch_sequences(headers):
 
 
 def set_search_params(
-    db=None,
+    database=None,
     smode=None,
     useid1=None,
     compbasedadj=None,
@@ -495,7 +324,7 @@ def set_search_params(
 ):
     """Set CD-Search search parameters."""
     params = {
-        "db": db,
+        "db": database,
         "smode": smode,
         "useid1": useid1,
         "compbasedadj": compbasedadj,
