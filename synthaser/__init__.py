30b731bef70fc2c90d3655050bd2233721a942cd
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-04-21 11:23:53 +0800

    update packaging files; use pyproject.toml and setup.cfg instead of setup.py

--- synthaser/__init__.py
+++ synthaser/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "1.1.7"
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass  # package not installed
