5bbe16ae73e0aeeb9b3c7e1defe40ecc0bd41bcb
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-04-23 13:12:43 +0800

    remove pytest config in setup.cfg

--- synthaser/settings.py
+++ synthaser/settings.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).parent.parent.resolve()
-RULE_FILE = ROOT_DIR / "synthaser" / "rules.json"
+ROOT_DIR = Path(__file__).parent.resolve()
+RULE_FILE = ROOT_DIR / "rules.json"
